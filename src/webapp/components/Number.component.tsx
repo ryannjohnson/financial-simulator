6ef5b23dd5d73a6076a30cb3a57fd1460a25071a
Ryan N Johnson <ryan@ryannjohnson.com>
2020-04-05 08:45:14 +0000

    Fix NumberComponent erasing significant zeros
    When a number like "1000" becomes "000", they used to be reduced to just
    one "0". Now, if the written value equals the number value, the text
    remains unchanged.

--- src/webapp/components/Number.component.tsx
+++ src/webapp/components/Number.component.tsx
@@ -14,13 +14,21 @@ const numberPattern = /^-?\d+(\.\d+)?$/;
 export default function NumberComponent({ min, setValue, step, value }: Props) {
   const [typedValue, setTypedValue] = React.useState(value.toString());
 
-  React.useEffect(() => setTypedValue(value.toString()), [value]);
+  React.useEffect(() => {
+    if (numberPattern.test(typedValue)) {
+      if (toNumber(typedValue, step) === value) {
+        return;
+      }
+    }
+
+    setTypedValue(value.toString());
+  }, [value]);
 
   return (
     <input
       className={styles.input}
       min={min}
-      onChange={event => {
+      onChange={(event) => {
         const { value: targetValue } = event.target;
 
         setTypedValue(targetValue);
@@ -29,19 +37,18 @@ export default function NumberComponent({ min, setValue, step, value }: Props) {
           return;
         }
 
-        let newValue: number;
-
-        if (step && step >= 1 && step % 1 === 0) {
-          newValue = parseInt(targetValue, 10);
-        } else {
-          newValue = parseFloat(targetValue);
-        }
-
-        setValue(newValue);
+        setValue(toNumber(targetValue, step));
       }}
       step={step}
       type="number"
       value={typedValue}
     />
   );
+}
+
+function toNumber(value: string, step: number | undefined): number {
+  if (step && step >= 1 && step % 1 === 0) {
+    return parseInt(value, 10);
+  }
+  return parseFloat(value);
 }
